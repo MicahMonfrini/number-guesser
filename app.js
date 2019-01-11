10fe87def5ad593e7d402507283e3fe21d9ed974
Micah Monfrini <ffrock905@gmail.com>
2019-01-10 20:56:09 -0600

    add ui element variables

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-// Game values
+// GAME VALUES
 let min = 1,
     max = 10,
     winningNum = 2,
     guessesLeft = 3;
 
+// UI ELEMENTS
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessNum = document.querySelector('guess-input'),
+      guessBtn = document.querySelector('#guess-btn'),
+      message = document.querySelector('.message');
