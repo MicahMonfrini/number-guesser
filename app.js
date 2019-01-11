772f04588867053fe7f26ed5600da5d337dd7656
Micah Monfrini <ffrock905@gmail.com>
2019-01-10 21:08:15 -0600

    add event listener for guess input w/validation

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ let min = 1,
 const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
-      guessNum = document.querySelector('guess-input'),
+      guessInput = document.querySelector('#guess-input'),
       guessBtn = document.querySelector('#guess-btn'),
       message = document.querySelector('.message');
 
 // Add min/max to UI
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Guess button event listner
+guessBtn.addEventListener('click', () => {
+ let guess = parseInt(guessInput.value);
+ // Validate input is a number within acceptable range
+ if (guess === Nan || guess < 1 || guess > 10) {
+  alert('Your guess must be a number between 1 and 10!')
+ }
+});
